8b36cd838567ee7c472e86c8c45c2c97585bd4b3
Ifejaye <iamifejaye@gmail.com>
2022-09-11 00:45:02 +0000

    attempted the optionals

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - prints text into standard output
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 60);
+
+	return (1);
+
+
+
+
+}
