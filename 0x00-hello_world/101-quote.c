6971fec6e99aa97792b9f65a963543c4391bd127
Ifejaye <iamifejaye@gmail.com>
2022-09-13 02:38:21 +0000

    done with hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,9 @@
  * main - prints text into standard output
  * Return: returns the value of 1
  */
-
 int main(void)
 {
-	write(2,and that piece of art is useful\" - Dora Korpar, 2015-10-19,60);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 
 	return (1);
-
-
-
-
 }
