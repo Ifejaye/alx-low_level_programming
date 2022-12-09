861976fd148e78fcfff59c2d0172e162993ac8ac
Ifejaye <iamifejaye@example.com>
2022-12-08 18:18:37 -0800

    attempt 1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+##include "lists.h"
+
+/**
+ * print_dlistint - prints all element of dll
+ * @dlistint: linked list
+ *
+ * Return: return number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (head != NULL)
+	{
+		printf("%d\n", head->data);
+		count++;
+		head = head->next;
+	}
+	return (count);
+}
