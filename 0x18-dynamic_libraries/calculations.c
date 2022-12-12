d84b7f8c5b51452e673f00ca25c3a60da55918ff
root <root@DESKTOP-DRLDR0N.localdomain>
2022-12-12 12:11:17 +0100

    commit 2

---
+++ 0x18-dynamic_libraries/calculations.c
@@ -0,0 +1,60 @@
+#include "calculations.h"
+/**
+  * add - sums two numbers
+  * @a: first number
+  * @b: second number
+  *
+  * Return: returns an integer
+  */
+int add(int a, int b)
+{
+	return(a + b);
+}
+
+
+/**
+  * sub - subtract two numbers
+  * @a: first number
+  * @b: second number
+  *
+  * Return: returns an integer
+  */
+int sub(int a, int b)
+{
+	return(a - b);
+}
+
+/**
+  * sub - subtract two numbers
+  * @a: first number
+  * @b: second number
+  *
+  * Return: returns an integer
+  */
+int mul(int a, int b)
+{
+	return(a * b);
+}
+
+/**
+  * div - divides two numbers
+  * @a: first number
+  * @b: second number
+  *
+  * Return: returns an integer
+  */
+int div(int a, int b)
+{
+	return(a / b);
+}
+/**
+  * mod - finds remainder of two numbers
+  * @a: first number
+  * @b: second number
+  *
+  * Return: returns an integer
+  */
+int mod(int a, int b)
+{
+	return(a % b);
+}
